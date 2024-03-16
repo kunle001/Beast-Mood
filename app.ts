a23481df75c12d84f41f03c3bc2c88086c8d28de
Olukeye <seunolukeye2000@mail.com>
2024-03-16 21:27:10 +0100

    adjustment

--- app.ts
+++ app.ts
@@ -1,10 +1,9 @@
 import express from 'express'
 import { AnimieRouter } from "./routes/animie.route"
-import UserRouter from "./routes/user.route"
 import AuthRouter  from "./routes/auth.route"
 import bodyParser from "body-parser"
-// import mongoSanitize from "mongo-sanitize"
-// import errorHandlerController from "./controllers/errorHandler.controller"
+import mongoSanitize from "mongo-sanitize"
+import errorHandlerController from "./controllers/errorHandler.controller"
 import AppError from "./utils/appError"
 
 // Register Routes
@@ -12,7 +11,7 @@ import AppError from "./utils/appError"
 const app= express()
 
 app.use(bodyParser.json())
-// app.use(mongoSanitize)
+app.use(mongoSanitize)
 
 
 
@@ -24,7 +23,7 @@ app.all('*', (req, res, next) => {
     next(new AppError(`Page ${req.originalUrl} is not found`, 404));
 });
 
-// app.use(errorHandlerController)
+app.use(errorHandlerController)
 
 // Export app
 export {app}
